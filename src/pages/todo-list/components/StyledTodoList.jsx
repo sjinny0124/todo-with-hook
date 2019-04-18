e378f57320ed111fab7305a1593710993bf2a319
sjinny0124 <sjinny0124@gmail.com>
2019-04-18 15:46:53 +0900

    TodoList useRef로 변경 커밋

--- src/pages/todo-list/components/StyledTodoList.jsx
+++ src/pages/todo-list/components/StyledTodoList.jsx
@@ -1,54 +1,57 @@
+import styled from 'styled-components';
 
-.TodoList {
-  margin: 20px;
-  border: 1px solid #ccc;
-  padding: 10px;
-  width: 600px;
-
-  .TodoListItem {
-    cursor: pointer;
+const StyledTodoList = styled.div`
+  .TodoList {
+    margin: 20px;
+    border: 1px solid #ccc;
     padding: 10px;
-    border-bottom: 1px dashed #ccc;
-    display: flex;
+    width: 600px;
 
+    .TodoListItem {
+      cursor: pointer;
+      padding: 10px;
+      border-bottom: 1px dashed #ccc;
+      display: flex;
 
-    &:last-child {
-      border-bottom: none;
-    }
-
-    &:hover {
-      background-color: aquamarine;
-    }
-
-    .col-ck-box {
-      width: 30px;
-    }
-
-    .col-content {
-      width: 100%;
-    }
-
-    .col-cmd-box {
-      width: 20px;
-      .btn {
-        cursor: pointer;
+      &:last-child {
+        border-bottom: none;
       }
 
-      .btn-edit {
-        display: none;
+      &:hover {
+        background-color: aquamarine;
+      }
+
+      .col-ck-box {
+        width: 30px;
+      }
+
+      .col-content {
+        width: 100%;
+      }
+
+      .col-cmd-box {
+        width: 20px;
+        .btn {
+          cursor: pointer;
+        }
+
+        .btn-edit {
+          display: none;
+        }
+      }
+    }
+
+    .footer {
+      display: flex;
+      justify-content: space-between;
+
+      div {
+        span {
+          margin-left: 10px;
+        }
       }
     }
   }
+`;
 
-  .footer {
-    display: flex;
-    justify-content: space-between;
-
-
-    div {
-      span {
-        margin-left: 10px;
-      }
-    }
-  }
-}
+export default StyledTodoList;
--- src/pages/todo-list/components/TodoList.jsx
+++ src/pages/todo-list/components/TodoList.jsx
@@ -1,34 +1,26 @@
-import React, { useState } from 'react';
+import React, {useRef} from 'react';
 import TodoListItem from './TodoListItem';
-import styled from "styled-components";
-import TodoListless from './TodoList.less';
+import StyledTodoList from './StyledTodoList.jsx';
 import {Input, Button, Icon} from 'antd';
 import PropTypes from 'prop-types';
 
-const Page = styled.div`
-TodoListless
-`;
-
 function TodoList(props) {
-
-  const {items, onCreate, onChangeComplete} = props;
+  const {
+    items,
+    onDelete,
+    onEdit,
+    onUnselectAll,
+    onSelectAll,
+    onCreate,
+    onChangeComplete,
+  } = props;
   const isSelectedAll = items.every(({completed}) => completed);
 
-  static propTypes = {
-    items: PropTypes.arrayOf(
-      PropTypes.shape({
-        name: PropTypes.string,
-        completed: PropTypes.bool,
-      })
-    ).isRequired,
-    onUnselectAll: PropTypes.func,
-  };
-
-  this.input = React.createRef();
+  const inputRef = useRef();
 
   const handleEnterTodoText = e => {
     onCreate(e.target.value);
-    this.input.current.setState({
+    inputRef.current.setState({
       value: '',
     });
   };
@@ -38,46 +30,57 @@ function TodoList(props) {
     onChangeComplete(index, item);
   };
 
-    return (
-      <Page className="TodoList">
-        <div>
-          <Input
-            ref={this.input}
-            onPressEnter={handleEnterTodoText}
-            addonAfter={<Icon type="plus" />}
-          />
-        </div>
+  return (
+    <div className="TodoList">
+      <div>
+        <Input
+          ref={inputRef}
+          onPressEnter={handleEnterTodoText}
+          addonAfter={<Icon type="plus" />}
+        />
+      </div>
+
+      <div>
+        {items.map((item, index) => {
+          return (
+            <TodoListItem
+              key={`item-${index}`}
+              {...item}
+              onDelete={e => onDelete(index)}
+              onEdit={value => onEdit(index, value)}
+              onChangeSelect={e =>
+                handleChangeComplteItem(item, index, e.target.checked)
+              }
+            />
+          );
+        })}
+      </div>
+
+      <div className={'footer'}>
+        {isSelectedAll ? (
+          <Button onClick={onUnselectAll}>선택해제</Button>
+        ) : (
+          <Button onClick={onSelectAll}>전체선택</Button>
+        )}
 
         <div>
-          {items.map((item, index) => {
-            return (
-              <TodoListItem
-                key={`item-${index}`}
-                {...item}
-                onDelete={e => onDelete(index)}
-                onEdit={value => onEdit(index, value)}
-                onChangeSelect={e =>
-                  handleChangeComplteItem(item, index, e.target.checked)
-                }
-              />
-            );
-          })}
+          <span>할일 {items.filter(item => !item.completed).length}</span>
+          <span>완료 {items.filter(item => item.completed).length}</span>
         </div>
-
-        <div className={'footer'}>
-          {isSelectedAll ? (
-            <Button onClick={onUnselectAll}>선택해제</Button>
-          ) : (
-            <Button onClick={onSelectAll}>전체선택</Button>
-          )}
-
-          <div>
-            <span>할일 {items.filter(item => !item.completed).length}</span>
-            <span>완료 {items.filter(item => item.completed).length}</span>
-          </div>
-        </div>
-      </Page>
-    );
+      </div>
+      <StyledTodoList />
+    </div>
+  );
 }
+
+TodoList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      completed: PropTypes.bool,
+    })
+  ).isRequired,
+  onUnselectAll: PropTypes.func,
+};
 
 export default TodoList;
